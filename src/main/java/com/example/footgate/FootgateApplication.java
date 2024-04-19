6b22e3cf3b379070adadd4398e0d02d83bbeded0
Quang9119 <nguyenminhquangquangquang@gmail.com>
2024-04-19 09:33:45 +0700

    complete

---
+++ src/main/java/com/example/footgate/FootgateApplication.java
@@ -0,0 +1,14 @@
+package com.example.footgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootgateApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(FootgateApplication.class, args);
+	}
+
+}
