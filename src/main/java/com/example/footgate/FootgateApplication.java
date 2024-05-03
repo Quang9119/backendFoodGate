83cdc105122933e6f7610a1d794a815712710f29
Quang9119 <nguyenminhquangquangquang@gmail.com>
2024-05-03 18:54:55 +0700

    hi

--- src/main/java/com/example/footgate/FootgateApplication.java
+++ src/main/java/com/example/footgate/FootgateApplication.java
@@ -1,15 +1,17 @@
 package com.example.footgate;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 
 @SpringBootApplication
 public class FootgateApplication {
